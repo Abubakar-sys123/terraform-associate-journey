824ce14f782f460376fa9dee875d7fc8b752e152
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-18 02:59:20 +0500

    Update variable.tf

--- Day01-Terraform-Overview/variable.tf
+++ Day01-Terraform-Overview/variable.tf
@@ -1,1 +1,9 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
 
+variable "bucket_name" {
+  description = "Unique name for the S3 bucket"
+  type        = string
+  defult = "my-modular-tf-bucket-00123"
+}
