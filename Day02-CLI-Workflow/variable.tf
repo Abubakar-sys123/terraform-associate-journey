866eefb899cb553dd3df04fa14408420091dbcdd
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-19 04:16:42 +0500

    Update variable.tf

--- Day02-CLI-Workflow/variable.tf
+++ Day02-CLI-Workflow/variable.tf
@@ -1,1 +1,28 @@
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316"
+}
+
+variable "key_name" {
+  default = "key.pem"
+}
+
+variable "ec2_name" {
+  default = "Day2Instance"
+}
 
