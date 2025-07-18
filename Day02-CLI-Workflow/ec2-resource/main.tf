5d97598126457fc98e2a3126c69675d412c715ae
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-19 04:19:53 +0500

    Update main.tf

--- Day02-CLI-Workflow/ec2-resource/main.tf
+++ Day02-CLI-Workflow/ec2-resource/main.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "web" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.sg_id]
-  associate_public_ip_address = true
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.sg_id]
+  key_name      = var.key_name
+
   tags = {
-    Name = "PublicEC2"
+    Name = var.ec2_name
   }
 }
