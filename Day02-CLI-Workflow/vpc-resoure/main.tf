69f3932bf457f0571a00d115f1ebf9b3709e1ca3
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-19 04:18:31 +0500

    Update main.tf

--- Day02-CLI-Workflow/vpc-resoure/main.tf
+++ Day02-CLI-Workflow/vpc-resoure/main.tf
@@ -1,44 +1,29 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "${var.project_name}-vpc"
-  }
+  cidr_block = var.vpc_cidr_block
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.project_name}-igw"
-  }
 }
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
-  availability_zone       = var.az
-  tags = {
-    Name = "${var.project_name}-public-subnet"
-  }
 }
 
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.project_name}-public-rt"
-  }
 }
 
-resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.public_rt.id
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-resource "aws_route_table_association" "public_assoc" {
+resource "aws_route_table_association" "public_subnet" {
   subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public_rt.id
+  route_table_id = aws_route_table.public.id
 }
-
