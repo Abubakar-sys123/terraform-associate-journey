a489fa5ccbb2cb1023d58142a9f4519443ca403f
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-19 04:17:52 +0500

    Update main.tf

--- Day02-CLI-Workflow/main.tf
+++ Day02-CLI-Workflow/main.tf
@@ -1,29 +1,26 @@
 provider "aws" {
   region = var.region
 }
-
 module "vpc" {
   source              = "./modules/vpc"
-  vpc_cidr            = var.vpc_cidr
+  vpc_cidr_block      = var.vpc_cidr_block
   public_subnet_cidr  = var.public_subnet_cidr
-  az                  = var.az
-  vpc_name            = var.vpc_name
+  availability_zone   = var.availability_zone
 }
 
 module "sg" {
-  source        = "./modules/security_group"
-  vpc_id        = module.vpc.vpc_id
-  sg_name       = var.sg_name
-  allowed_ssh   = var.allowed_ssh
-  allowed_http  = var.allowed_http
+  source = "./modules/sg"
+  vpc_id = module.vpc.vpc_id
 }
 
 module "ec2" {
-  source         = "./modules/ec2"
-  ami            = var.ami
-  instance_type  = var.instance_type
-  subnet_id      = module.vpc.subnet_id
-  sg_id          = module.sg.sg_id
-  name           = var.instance_name
+  source           = "./modules/ec2"
+  ami_id           = var.ami_id
+  instance_type    = var.instance_type
+  subnet_id        = module.vpc.public_subnet_id
+  sg_id            = module.sg.sg_id
+  key_name         = var.key_name
+  ec2_name         = var.ec2_name
 }
+
 
