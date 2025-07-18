3bfd4b58f4961ccc8ab2a96d017494ef38909ef5
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-19 04:11:07 +0500

    Update main.tf

--- Day02-CLI-Workflow/sg-resource/main.tf
+++ Day02-CLI-Workflow/sg-resource/main.tf
@@ -1,13 +1,20 @@
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "web_sg" {
+  name        = var.sg_name
+  description = "Allow HTTP and SSH"
   vpc_id      = var.vpc_id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.allowed_ssh
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_http
   }
 
   egress {
@@ -15,5 +22,9 @@ resource "aws_security_group" "allow_ssh" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_name
   }
 }
