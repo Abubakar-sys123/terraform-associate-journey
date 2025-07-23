13b069239ecf41099b2e21f5309577d9178fdda8
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-23 10:27:11 +0500

    Update variable.tf

--- Web-Server-Provisioning/variable.tf
+++ Web-Server-Provisioning/variable.tf
@@ -1,1 +1,16 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_id" {
+  description = "VPC ID to deploy resources into"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH key name to use for EC2 login"
+}
 
