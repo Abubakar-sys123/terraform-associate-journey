87b2a707a6a61e366dc6e0e2934f7be331156bd1
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-24 11:43:05 +0500

    Update main.tf

--- terraform-Day04/ec2-resource/main.tf
+++ terraform-Day04/ec2-resource/main.tf
@@ -1,1 +1,33 @@
 
+resource "aws_instance" "public_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.public_subnet_id
+  vpc_security_group_ids = [var.sg_id]
+  associate_public_ip_address = true
+  key_name = var.key_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl enable httpd
+              systemctl start httpd
+              echo "<h1>Public EC2 - Web Server</h1>" > /var/www/html/index.html
+              EOF
+}
+
+resource "aws_instance" "private_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.private_subnet_id
+  vpc_security_group_ids = [var.sg_id]
+  key_name = var.key_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y awscli
+              aws s3 ls
+              EOF
+}
