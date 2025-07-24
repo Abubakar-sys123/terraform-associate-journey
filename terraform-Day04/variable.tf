5e081f2a11a3ffe049fc19b7dcdd7da8ac83b015
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-24 11:36:39 +0500

    Update variable.tf

--- terraform-Day04/variable.tf
+++ terraform-Day04/variable.tf
@@ -1,1 +1,42 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "day4-project-bucket-abubakar"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
 
