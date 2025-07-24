86692ec6feb04aacf06a5ed9a2af4eaa06a08253
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-24 11:30:30 +0500

    Update main.tf

--- terraform-Day04/main.tf
+++ terraform-Day04/main.tf
@@ -1,1 +1,33 @@
+module "vpc" {
+  source = "./modules/vpc"
+  vpc_cidr_block = var.vpc_cidr_block
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+}
 
+module "security_group" {
+  source = "./modules/security_group"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "s3_bucket" {
+  source      = "./modules/s3_bucket"
+  bucket_name = var.bucket_name
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+  public_subnet_id  = module.vpc.public_subnet_id
+  private_subnet_id = module.vpc.private_subnet_id
+  sg_id             = module.security_group.sg_id
+  instance_type     = var.instance_type
+  ami_id            = var.ami_id
+  key_name          = var.key_name
+}
+
+module "s3_endpoint" {
+  source = "./modules/s3_endpoint"
+  vpc_id         = module.vpc.vpc_id
+  subnet_ids     = [module.vpc.private_subnet_id]
+  route_table_id = module.vpc.private_route_table_id
+}
